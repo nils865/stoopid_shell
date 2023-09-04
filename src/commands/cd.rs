6877a2a980966c41b3139c5aef13c6e8b7b18835
Nils <nils865@users.noreply.github.com>
2023-09-04 19:44:36 +0200

    Implemented cd

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,3 +1,16 @@
+use std::{env, path::Path};
+
 pub fn cmd_cd(args: &Vec<String>) -> i8 {
-    return 1;
+    if args.len() == 0 {
+        env::set_current_dir(Path::new(env::var("HOME").unwrap().as_str())).unwrap();
+        return 0;
+    }
+
+    match env::set_current_dir(Path::new(args[0].as_str())) {
+        Ok(_) => return 0,
+        Err(_) => {
+            println!("cd: {}: No such file or directory", args[0]);
+            return 1;
+        }
+    }
 }
