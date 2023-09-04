c246c4605e38ec08035b2f7cdd4bc028275e8f90
Nils <nils865@users.noreply.github.com>
2023-09-04 17:56:20 +0200

    Added an exit command

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ fn main() {
     let prompt = "➜  ";
 
     while running {
-        let mut user_input = String::new();
+        let mut command = String::new();
 
         print!("{}", prompt);
         stdout().flush().expect("Failed to flush stdout");
         stdin()
-            .read_line(&mut user_input)
+            .read_line(&mut command)
             .expect("Failed to read line");
+
+        let args: Vec<&str> = command.trim().split(" ").collect();
+
+        match args[0] {
+            "exit" => return,
+            _ => println!("Command not found: {}", args[0]),
+        }
     }
 }
