2c99686dc07f239067e4e33b49fda32e5c5eb620
Nils <nils865@users.noreply.github.com>
2023-09-04 19:25:23 +0200

    fixed prompt

--- src/prompt.rs
+++ src/prompt.rs
@@ -5,9 +5,9 @@ pub fn get_prompt(exit_code: i8) -> String {
     let mut prompt = String::new();
 
     if exit_code == 0 {
-        prompt.push_str(&"➜ ".green().to_string());
+        prompt.push_str(&"➜  ".green().to_string());
     } else {
-        prompt.push_str(&"➜ ".red().to_string());
+        prompt.push_str(&"➜  ".red().to_string());
     }
 
     prompt.push_str(
