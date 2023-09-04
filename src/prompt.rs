2d4f2c7d964ddb7e871df51b36ec2ac95fb1e4f2
Nils <nils865@users.noreply.github.com>
2023-09-04 19:24:18 +0200

    "Added directory to prompt

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,4 +1,5 @@
 use colored::Colorize;
+use std::env;
 
 pub fn get_prompt(exit_code: i8) -> String {
     let mut prompt = String::new();
@@ -9,5 +10,20 @@ pub fn get_prompt(exit_code: i8) -> String {
         prompt.push_str(&"➜ ".red().to_string());
     }
 
-    return prompt;
+    prompt.push_str(
+        &format!(
+            "{} ",
+            env::current_dir()
+                .expect("Failed to get current directory")
+                .display()
+                .to_string()
+                .split("/")
+                .last()
+                .unwrap()
+        )
+        .bright_cyan()
+        .to_string(),
+    );
+
+    return prompt.bold().to_string();
 }
