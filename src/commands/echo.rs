c9c9777573f5cb739b2c7b7627b64fa1d8e9f568
Nils <nils865@users.noreply.github.com>
2023-09-05 19:12:33 +0200

    Added support for env variables in echo command

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -1,5 +1,17 @@
+use std::env;
+
 pub fn cmd_echo(args: &Vec<String>) -> i8 {
-    let text: String = args.join(" ");
+    let mut text: String = String::new();
+
+    for arg in args {
+        let mut val: String = arg.clone();
+
+        if arg.starts_with("$") {
+            val = env::var(arg.strip_prefix("$").unwrap_or("")).unwrap_or(String::new());
+        }
+
+        text = String::from(format!("{} {}", text, val));
+    }
 
     println!("{}", text);
 
