3d75f6424a8c283dd6f031572e5fdbe02898ed2d
Nils <nils865@users.noreply.github.com>
2023-09-06 23:35:36 +0200

    Improved how inputs are being read

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::{
-    io::{stdin, stdout, Write},
-    process,
-};
+use std::process;
 
 mod commands;
 mod prompt;
 
 use commands::input_handler;
 use prompt::get_prompt;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
 fn main() {
     let running = true;
+    let mut rl = DefaultEditor::new().expect("Failed to create readline");
 
     /*
     * 0: Success
@@ -21,13 +20,15 @@ fn main() {
     let mut exit_code: i8 = 0;
 
     while running {
-        let mut command = String::new();
-
-        print!("{}", get_prompt(exit_code));
-        stdout().flush().expect("Failed to flush stdout");
-        stdin()
-            .read_line(&mut command)
-            .expect("Failed to read line");
+        let command: String = match rl.readline(&get_prompt(exit_code)) {
+            Ok(l) => {
+                let _ = rl.add_history_entry(l.as_str());
+                l
+            }
+            Err(ReadlineError::Interrupted) => String::new(),
+            Err(ReadlineError::Eof) => process::exit(0),
+            Err(_) => String::new(),
+        };
 
         let args: Vec<&str> = command.trim().split(" ").collect();
 
